782dfeb2f92e27603ba0772dc32a70a6dcd0fc28
Adolfo <166766031+AdolfotULS@users.noreply.github.com>
2025-09-24 10:23:11 -0300

    Add project files.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "KSM_ULS")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "KSM_ULS.Pages")]
