ec8aa5f8d6ebf867284e0b7ce7978822bbf812d8
Astrado_DGM <patricio.vejar@userena.cl>
2025-09-25 18:26:37 -0300

    Added Configuration And Close Session buttons and PopUp Menu for Configuration
    Añadido dos botones y sus respectivas clases y llamadas, añadido menu Pop up para llenar con las opciones de configuracion respectivas a futuro

--- Pages/DashboardPage.xaml.cs
+++ Pages/DashboardPage.xaml.cs
@@ -1,3 +1,7 @@
+using CommunityToolkit.Maui.Extensions;
+using KSM_ULS.Views.Config_options;
+using System.Threading.Tasks;
+
 namespace KSM_ULS.Pages;
 
 public partial class DashboardPage : ContentPage
@@ -13,4 +17,15 @@ public partial class DashboardPage : ContentPage
 
     void OnClientesClicked(object sender, EventArgs e) => Host.Content = new Views.DashClientsView();
     //void OnReportesClicked(object sender, EventArgs e) => Host.Content = new Views.DashReportsView();
+
+    async void OnConfigClicked(object sender, EventArgs e)
+    {
+        var popupConfigMenu = new popUpConfig(); //abre un menu pop up para la configuracion
+        await this.ShowPopupAsync(popupConfigMenu);
+    }
+    // Cuando se presiona el boton de config abre menu popup con todas las opciones de configuracion respectivas 
+
+    void OnCloseSessionClicked(object sender, EventArgs e) { }
+    //cierra sesion -- envia al usuario a la pantalla de login
+    // considerar, que tenemos que borrar cualquier credencial de usuario de sale de la cuenta
 }
--- Views/Config-options/popUpConfig.xaml.cs
+++ Views/Config-options/popUpConfig.xaml.cs
@@ -1,9 +1,20 @@
+using CommunityToolkit.Maui.Views;
+
 namespace KSM_ULS.Views.Config_options;
 
-public partial class popUpConfig : ContentPage
+public partial class popUpConfig : Popup
 {
 	public popUpConfig()
 	{
 		InitializeComponent();
+	}
+	void onClickButton1(object sender, EventArgs e) { }//cambiar nombre a uso respectivo
+
+    void onClickButton2(object sender, EventArgs e) { }//cambiar nombre a uso respectivo
+
+    void onClickButton3(object sender, EventArgs e) { }//cambiar nombre a uso respectivo
+    
+    void onClickClosePopUp(object sender, EventArgs e) {
+		CloseAsync(); //cierra el pup up apropiadamente
 	}
 }
